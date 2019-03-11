381725fa17b0c58708842c92a2b4135e79075223
Meghana Putta <meghanareddy.putta@gmail.com>
2019-03-11 18:33:44 -0500

    updated main

--- Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/MainActivity.java
+++ Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/MainActivity.java
@@ -9,13 +9,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });}
+            public void test(){
 
-    }
+                Intent i=new Intent(this, Activity2.class);
+                startActivity(i);
+        }
+
+
 }
 
