412e297bf1aeba7eb74db2f3d1c5a7c39c053f10
Himabindu Poshala <himabindu.poshala23@gmail.com>
2019-03-11 21:14:20 -0500

    activity1files

--- Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity1.java
+++ Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity1.java
@@ -1,4 +1,28 @@
 package com.example.androidprojectgroup_2_6;
 
-public class Activity1 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity1 extends AppCompatActivity {
+    Button but;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1);
+
+        but=(Button)findViewById(R.id.Button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });}
+    public void test(){
+
+        Intent i=new Intent(this, Activity2.class);
+        startActivity(i);
+    }
 }
--- Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity1_1.java
+++ Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity1_1.java
@@ -1,4 +1,28 @@
 package com.example.androidprojectgroup_2_6;
 
-public class Activity1_1 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity1_1 extends AppCompatActivity {
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_1);
+
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });}
+    public void test(){
+
+        Intent i=new Intent(this, Activity2.class);
+        startActivity(i);
+    }
 }
