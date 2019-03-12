fd49479383744782313a20c6c20230b5008e94c6
Zaiba <s.zaiba.ali@gmail.com>
2019-03-11 19:54:03 -0500

    activity3 with comments

--- Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity3.java
+++ Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity3.java
@@ -1,15 +1,38 @@
 package com.example.androidprojectgroup_2_6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Activity3 extends AppCompatActivity {
+    Button button1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
 
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                instance();
+
+            }
+        });
+    }
+    public void instance()
+    {
+        Intent intent = new Intent(this, Activity3_1.class);
+        startActivity(intent);
 
 
     }
---
+++ Androidprojectgroup_26/app/src/main/java/com/example/androidprojectgroup_2_6/Activity3_1.java
@@ -0,0 +1,13 @@
+package com.example.androidprojectgroup_2_6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity3_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity3_1);
+    }
+}
