0a97bb597425c09fb195c882f0ed55a7b083741d
Himabindu Poshala <himabindu.poshala23@gmail.com>
2019-04-02 16:21:04 -0500

    Updated the file in Session/MyApplication.java

--- project2-6/app/src/main/java/com/example/androidprojectgroup_26/Session/MyApplication.java
+++ project2-6/app/src/main/java/com/example/androidprojectgroup_26/Session/MyApplication.java
@@ -1,4 +1,48 @@
 package com.example.androidprojectgroup_26.Session;
 
-public class MyApplication {
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    String SelectedFoodCenterName, SelectedFoodCenterID;
+    int SelectedFoodCenterImage;
+    com.example.androidprojectgroup_2_6.Objects.user user = null;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public String getSelectedFoodCenterName() {
+        return SelectedFoodCenterName;
+    }
+
+    public void setSelectedFoodCenterName(String selectedFoodCenterName) {
+        SelectedFoodCenterName = selectedFoodCenterName;
+    }
+
+    public String getSelectedFoodCenterID() {
+        return SelectedFoodCenterID;
+    }
+
+    public void setSelectedFoodCenterID(String selectedFoodCenterID) {
+        SelectedFoodCenterID = selectedFoodCenterID;
+    }
+
+    public int getSelectedFoodCenterImage() {
+        return SelectedFoodCenterImage;
+    }
+
+    public void setSelectedFoodCenterImage(int selectedFoodCenterImage) {
+        SelectedFoodCenterImage = selectedFoodCenterImage;
+    }
+
+    public com.example.androidprojectgroup_2_6.Objects.user getUser() {
+        return user;
+    }
+
+    public void setUser(com.example.androidprojectgroup_2_6.Objects.user user) {
+        this.user = user;
+    }
 }
