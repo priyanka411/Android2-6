b0c31860c826f64a671e2a6908b1e13d5165c3ad
Zaiba <s.zaiba.ali@gmail.com>
2019-04-04 16:07:08 -0500

    Wrote code in Splash.java

--- project2-6/app/src/main/java/com/example/androidprojectgroup_26/UI/Splash.java
+++ project2-6/app/src/main/java/com/example/androidprojectgroup_26/UI/Splash.java
@@ -1,4 +1,27 @@
 package com.example.androidprojectgroup_26.UI;
 
 
-public class Splash {}
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.androidprojectgroup_2_6.R;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        }, 2000);
+    }
+}
